faf04713a53a2058e4f13740a9f8f08399064a2a
Andriy Andrunevchyn <andriy.andrunevchyn@emilfrey.hr>
2023-02-15 14:06:05 +0200

    parse date with provided time zone

---
+++ src/main/java/com/andrunevchyn/obvious/DateTimeParserTest.java
@@ -0,0 +1,18 @@
+package com.andrunevchyn.obvious;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+
+public class DateTimeParserTest {
+
+    public static void main(String[] args) {
+        String input = "2023-02-16T02:10:00Z";
+        DateTimeFormatter  formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'").withZone(TimeZone.getTimeZone("Europe/Copenhagen").toZoneId());
+
+        ZonedDateTime then = ZonedDateTime.parse(input, formatter );
+
+        System.out.println(then);
+    }
+}
